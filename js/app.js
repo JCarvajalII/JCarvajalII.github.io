8976bb6b623b3c72a7088490ca5c89f661580c19
Jaime Alejandro A. Carvajal II <2161046@slu.edu.ph>
2018-04-12 19:41:06 +0800

    PWA Webiste first commit
    PWA website with search engine and notification

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function searchFunction() {
+    var input, filter, ul, li, h2, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        h2 = li[i].getElementsByTagName("h2")[0];
+        if (h2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+}

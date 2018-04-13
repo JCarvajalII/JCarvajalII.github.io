e13fc19effd95ac5802cad5065fc6626892e6aa3
Jaime Alejandro A. Carvajal II <2161046@slu.edu.ph>
2018-04-14 02:26:56 +0800

    updated

--- js/app.js
+++ js/app.js
@@ -13,3 +13,49 @@ function searchFunction() {
         }
     }
 }
+
+//IndexedDB
+// name, email, phonenumber
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+// Open (or create) the database
+var mydb = window.indexedDB.open("PWADB", 1), db, tx, store, index;
+
+mydb.onupgradeneeded = function() {
+    db = mydb.result,
+        store = db.createObjectStore("customerSupport", {keypath: id, autoIncrement: true}), 
+}
+mydb.onerror = function(e) {
+    console.log("There was an error" + e.target.errorCode);
+}
+mydb.onsuccess = function(e) {
+    console.log("Success");
+    db = mydb.result;
+}
+    
+function addCustomer() {
+    var name = document.getElementById("cName");
+    var email = document.getElementById("cEmail");
+    var number = document.getElementById("phNumber");
+    
+    tx = db.transaction(["customerSupport"], "readwrite")
+    store = transaction.objectStore("customerSupport");
+    
+    var customer = {
+        name: name,
+        email: email,
+        number: number
+    }
+    var request = store.add(customer);
+    
+    request.onsuccess = function(e) {
+        request.location.href="index.html";
+        alert("Thank you. Your information was sent successfully. We will contact you asap.");
+    }
+    
+    request.onerror = function(e){
+        alert("Error");
+    }
+    
+}
+

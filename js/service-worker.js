ef4df8497dfe3444845261f9e36cce08382d4305
Jaime Alejandro A. Carvajal II <2161046@slu.edu.ph>
2018-04-13 19:22:43 +0800

    updated serviceworker

--- js/service-worker.js
+++ js/service-worker.js
@@ -3,7 +3,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  './', '/index.html', '/js/app.js' // Alias for index.html
+  '/index.html', '/js/app.js', './' // Alias for index.html
 ];
 
 // The install handler takes care of precaching the resources we always need.

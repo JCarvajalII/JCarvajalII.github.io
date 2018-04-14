891d3961d343cabc903b462c643d4bca3487c5f1
Jaime Alejandro A. Carvajal II <2161046@slu.edu.ph>
2018-04-14 08:16:04 +0800

    asd

--- js/app.js
+++ js/app.js
@@ -13,3 +13,50 @@ function searchFunction() {
         }
     }
 }
+
+//IndexedDB
+var request = indexedDB.open("PWADB", 1);
+
+request.onsuccess = function(e) {
+    console.log("Success. Opened Database..");
+    db = e.target.result;
+}
+
+request.onerror = function(e) {
+    console.log("Error on opening database...");
+}
+
+request.onupgradeneeded = function(e) {
+    var db = e.target.result;
+    
+    if(!db.objectStoreNames.contains('customer')) {
+        var os = db.createObjectStore('customer', {keyPath: "id", autoIncrement: true});
+    }
+}
+
+function addCustomer() {
+    var name = document.getElementById("cName").value;
+    var email = document.getElementById("cEmail").value;
+    var number = document.getElementById("phNumber").value;
+    
+    var tx = db.transaction(["customer"], "readwrite");
+    var store = tx.objectStore("customer");
+    
+    var customer = {
+        name: name,
+        email: email,
+        number: number
+    }
+    var request = store.add(customer);
+    
+    request.onsuccess = function(e) {
+        window.location.href="index.html";
+        console.log("Thank you. Your information was sent successfully. We will contact you asap.");
+        alert("Thank you for your time. We will contact you as soon as possible.");
+    }
+    
+    request.onerror = function(e){
+        console.log("Error");
+    }
+    
+}
